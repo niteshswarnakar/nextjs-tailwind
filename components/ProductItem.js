da8d9cb91579ba98662c8f22f6af121cdd5cfba3
niteshswarnakar <nitesh.swarnakar1@gmail.com>
2022-11-29 07:46:36 +0545

    added image,  utils folder with data for product

---
+++ components/ProductItem.js
@@ -0,0 +1,32 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import Link from "next/link";
+function ProductItem({ product }) {
+  return (
+    <div className="card">
+      <Link href={`/product/${product.slug}`} legacyBehavior>
+        <a>
+          <img
+            src={product.image}
+            alt={product.name}
+            className="rounded shadow"
+          />
+        </a>
+      </Link>
+      <div className="flex flex-col items-center justify-center p-5">
+        <Link href={`/product/${product.slug}`} legacyBehavior>
+          <a>
+            <h2 className="text-lg"></h2>
+          </a>
+        </Link>
+        <p className="mb-2">{product.brand}</p>
+        <p>${product.price}</p>
+        <button className="primary-button" type="button">
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ProductItem;
